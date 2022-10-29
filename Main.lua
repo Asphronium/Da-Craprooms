909ed5ad749e128cbc60196093c8cf6ff51f0dad
Asphronium Ion <115331678+Asphronium@users.noreply.github.com>
2022-10-28 21:12:42 -0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,8 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Luxware-UI-Library/main/Source.lua"))()
+
+local window = lib.CreateWindow("Da-Backrooms")
+
+local bruh = window:Tab("IMPORTANT")
+local bruh1 = bruh:Section("READ")
+
+bruh1:Label("some dumbass decided to alert\nthe devs on my script in the discord server.\nthe script will be up soon.")
