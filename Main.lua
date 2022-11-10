5a683b0711b40e6164f1841ee783a04ab01181e1
Asphronium Ion <115331678+Asphronium@users.noreply.github.com>
2022-11-09 22:38:24 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,3 +4,8 @@ CoreGui:SetCore("SendNotification", {
     Title = "Hey! Brand new AC!!!",
     Text = "Current script is patched. working on bypassing this anti cheat lol (contact me @ Davy#1374 if you have any questions). This COULD take a day or two depending on my schedule."
 })
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Hey again.",
+    Text = "I'll made a lot more features afterwards. Bobat is retarded! remember that :)"
+})
