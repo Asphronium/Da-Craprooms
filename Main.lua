593ce6c6f9d8baebb65b2c567c12090801e953a8
Asphronium Ion <115331678+Asphronium@users.noreply.github.com>
2022-11-09 22:37:04 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,5 +2,5 @@ local CoreGui = game:GetService("StarterGui")
 
 CoreGui:SetCore("SendNotification", {
     Title = "Hey! Brand new AC!!!",
-    Text = "Current script is patched. working on bypassing this anti cheat lol (contact me @ Davy#1374 if you have any questions)."
+    Text = "Current script is patched. working on bypassing this anti cheat lol (contact me @ Davy#1374 if you have any questions). This COULD take a day or two depending on my schedule."
 })
